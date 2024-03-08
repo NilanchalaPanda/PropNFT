98d638deb326273236803eca8e282bce5c440986
Nilanchal <nilanchalpanda2003@gmail.com>
2024-03-08 11:40:09 +0530

    Navbar, hero section and property card added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+//Images - 
+import house from "./assets/houses.png";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
-
   return (
     <div>
+      <Navigation />
+      <Search />
+      <div className="cards__section">
+        <h3>Homes for you</h3>
 
-      <div className='cards__section'>
+        <hr />
 
-        <h3>Welcome to Millow</h3>
-
+        <div className="cards">
+          <div className="card">
+            <div className="card__image">
+              <img src={house} alt="Home" />
+            </div>
+            <div className="card__info">
+              <h4>1 ETH</h4>
+              <p>
+                <strong>1</strong> bed | 
+                <strong>{" "}2</strong> bath  | 
+                <strong>{" "} 300</strong> sqft
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,30 @@
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg";
 
 const Navigation = ({ account, setAccount }) => {
+  return (
+    <nav>
+      <div className="nav__brand">
+        <a href="/">
+          <img src={logo} alt="LOGO " />
+        </a>
+      </div>
+      <ul className="nav__links">
+        <li>
+          <a href="#">BUY</a>
+        </li>
+        <li>
+          <a href="#">SELL</a>
+        </li>
+        <li>
+          <a href="#">RENT</a>
+        </li>
+      </ul>
 
-}
+      <button className="nav__connect" type="button">
+        0x0....
+      </button>
+    </nav>
+  );
+};
 
 export default Navigation;
