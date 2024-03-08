685b2afd5de771348bc37ba85e6e817b27a76f1e
Nilanchal <nilanchalpanda2003@gmail.com>
2024-03-08 08:44:09 +0530

    Approval of all parties [Seller, Lender, Buyer]  implemented

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -16,13 +16,19 @@ contract Escrow {
     address payable public seller;
     address public nftAddress;
 
+
+    modifier onlyBuyer(uint _nftID){
+        require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+        _;
+    }
+
     modifier onlySeller() {
         require(msg.sender == seller, "Only seller can call this method");
         _;
     }
 
-    modifier onlyBuyer(uint _nftID){
-        require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+    modifier onlyInspector(){
+        require(msg.sender == inspector, "Only Inspector can call this method");
         _;
     }
 
@@ -31,6 +37,8 @@ contract Escrow {
     mapping(uint256 => uint256) public purchasePrice;
     mapping(uint256 => uint256) public escrowAmount;
     mapping(uint256 => address) public buyer;
+    mapping(uint256 => bool) public inspectionPassed;
+    mapping(uint256 => mapping(address => bool)) public approval;
 
     constructor(address _nftAdd, address payable _seller, address _inspector, address _lender) {
         nftAddress = _nftAdd;
@@ -58,6 +66,15 @@ contract Escrow {
     function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID){
         // EscrowAmount is the money have to put in for the transaction of the property -
         require(msg.value >= escrowAmount[_nftID], "Insufficient funds");
+    }
+
+    // INSPECTOR - 
+    function updateInspectionStatus(uint256 _nftID, bool _status) public onlyInspector() {
+        inspectionPassed[_nftID] = _status;
+    }
+
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
     }
 
     receive() external payable {}
